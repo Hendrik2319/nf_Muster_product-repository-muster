481378193a55d300dbf499d7094ffe1f0fb12ba4
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-09-18 12:02:49 +0200

    feat: added tests for ProductService.getProduct/.removeProduct/.updateProduct

--- src/test/java/com/example/productrepository/ProductServiceTest.java
+++ src/test/java/com/example/productrepository/ProductServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -62,14 +63,141 @@ class ProductServiceTest {
     }
 
     @Test
-    void getProduct() {
+    void whenGetProduct_getsInvalidID_returnEmptyOptional() {
+        // Given
+        when(productRepository.findById("456")).thenReturn(
+                Optional.empty()
+        );
+
+        // When
+        Optional<Product> actual = productService.getProduct("456");
+
+        // Then
+        verify(productRepository).findById("456");
+        Optional<Product> expected = Optional.empty();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void whenGetProduct_getsValidID_returnCorrespondingProduct() {
+        // Given
+        when(productRepository.findById("456")).thenReturn(
+                Optional.of(
+                        new Product("456","Title1",12)
+                )
+        );
+
+        // When
+        Optional<Product> actual = productService.getProduct("456");
+
+        // Then
+        assertNotNull(actual);
+        assertTrue(actual.isPresent());
+
+        verify(productRepository).findById("456");
+        Product expected = new Product("456","Title1",12);
+        assertEquals(expected, actual.get());
     }
 
     @Test
     void removeProduct() {
+        // Given
+
+        // When
+        productService.removeProduct("456");
+
+        // Then
+        verify(productRepository).deleteById("456");
     }
 
     @Test
-    void updateProduct() {
+    void whenUpdateProduct_getsInvalidID_returnEmptyOptional() {
+        // Given
+        when(productRepository.findById("456")).thenReturn(
+                Optional.empty()
+        );
+
+        // When
+        Optional<Product> actual = productService.updateProduct("456", "dsdsf", 12);
+
+        // Then
+        verify(productRepository).findById("456");
+        Optional<Product> expected = Optional.empty();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void whenUpdateProduct_getsValidID_returnUpdatedProduct() {
+        // Given
+        when(productRepository.findById("456")).thenReturn(
+                Optional.of(
+                        new Product("456","Title1",45)
+                )
+        );
+        when(productRepository.save(new Product("456","dsdsf",12))).thenReturn(
+                new Product("456","dsdsf",12)
+        );
+
+        // When
+        Optional<Product> actual = productService.updateProduct("456", "dsdsf", 12);
+
+        // Then
+        assertNotNull(actual);
+        assertTrue(actual.isPresent());
+
+        verify(productRepository).findById("456");
+        verify(productRepository).save(new Product("456","dsdsf",12));
+        Product expected = new Product("456","dsdsf",12);
+        assertEquals(expected, actual.get());
+    }
+
+    @Test
+    void whenUpdateProduct_getsValidIDAndTitle_returnUpdatedProduct() {
+        // Given
+        when(productRepository.findById("456")).thenReturn(
+                Optional.of(
+                        new Product("456","Title1",45)
+                )
+        );
+        when(productRepository.save(new Product("456","dsdsf",45))).thenReturn(
+                new Product("456","dsdsf",45)
+        );
+
+        // When
+        Optional<Product> actual = productService.updateProduct("456", "dsdsf", null);
+
+        // Then
+        assertNotNull(actual);
+        assertTrue(actual.isPresent());
+
+        verify(productRepository).findById("456");
+        verify(productRepository).save(new Product("456","dsdsf",45));
+        Product expected = new Product("456","dsdsf",45);
+        assertEquals(expected, actual.get());
+    }
+
+    @Test
+    void whenUpdateProduct_getsValidIDAndPrice_returnUpdatedProduct() {
+        // Given
+        when(productRepository.findById("456")).thenReturn(
+                Optional.of(
+                        new Product("456","Title1",45)
+                )
+        );
+        when(productRepository.save(new Product("456","Title1",12))).thenReturn(
+                new Product("456","Title1",12)
+        );
+
+        // When
+        Optional<Product> actual = productService.updateProduct("456", null, 12);
+
+        // Then
+        assertNotNull(actual);
+        assertTrue(actual.isPresent());
+
+        verify(productRepository).findById("456");
+        verify(productRepository).save(new Product("456","Title1",12));
+        Product expected = new Product("456","Title1",12);
+        assertEquals(expected, actual.get());
     }
 }
